fb45a8debe4c26f6dad36209f5bb2e9062d4e0c2
salwaaprsn <161547820+salwaaprsn@users.noreply.github.com>
2025-02-26 18:37:46 +0700

    Add files via upload

---
+++ BangunDatar.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ti23e;
+
+/**
+ *
+ * @author komputer 35
+ */
+public class BangunDatar {
+    int panjang;
+    int lebar;
+    
+    public BangunDatar(int panjang, int lebar){
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
+    public void setBangunDatar(int panjang, int lebar){
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
+    public int getPanjang(){
+        return panjang;
+    }
+    
+    public int getLebar(){
+        return lebar;
+    }
+}
